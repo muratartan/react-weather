114eca84e410c1470e7051647ac696a43f1d961b
murat <muratartan@hotmail.com>
2020-11-24 20:04:03 +0300

    set weather component layout

--- src/containers/Weather/Weather.js
+++ src/containers/Weather/Weather.js
@@ -3,13 +3,19 @@ import React, {Component} from 'react';
 import './Weather.css';
 import CurrentWeather from '../../components/CurrentWeather/CurrentWeather';
 import Forecast from '../../components/Forecast/Forecast';
+import Input from '../../components/Input/Input';
+import Button from '../../components/Buttons/Button/Button';
 
 class Weather extends Component {
     render () {
         return (
             <div className='Weather'>
-                <div>Current Weather</div>
-                <div>Forecast</div>
+               <div className='WeatherLeftLayout'>
+                   <Input />
+                    <div>Current Weather</div>
+                    <div>Favourites</div>
+               </div>
+               <div className='WeatherRightLayout'>forecast</div>
             </div>
         );
     }
