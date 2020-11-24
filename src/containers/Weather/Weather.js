991297ea69f3062746500337728fa5affe47446e
murat <muratartan@hotmail.com>
2020-11-24 17:22:04 +0300

    set weather component and its style

--- src/containers/Weather/Weather.js
+++ src/containers/Weather/Weather.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
 
+import './Weather.css';
+import CurrentWeather from '../../components/CurrentWeather/CurrentWeather';
+import Forecast from '../../components/Forecast/Forecast';
+
 class Weather extends Component {
     render () {
         return (
-            <div>
+            <div className='Weather'>
                 <div>Current Weather</div>
                 <div>Forecast</div>
             </div>
