ee569f064b852e45353564ba853a0f7164508053
murat <muratartan@hotmail.com>
2020-11-24 13:20:08 +0300

    set input styling

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -4,7 +4,12 @@ import './Input.css';
 
 const input = props => (
 
-    <input className='Input' onChange={props.changed} value={props.value} />
+    <input 
+        className='Input' 
+        onChange={props.changed} 
+        value={props.value}
+        placeholder={props.placeholder}
+     />
 );
 
 export default input;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
     render () {
         return (
             <div className='Home'>
-                <Input/>
+                <Input placeholder='Search location...' />
             </div>
         );
     }
