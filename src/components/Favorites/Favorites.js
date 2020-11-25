2dab9361f3e859dba70f9a0ddb621effbd9283a9
murat <muratartan@hotmail.com>
2020-11-25 11:55:14 +0300

    add remove button to favorite locations

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import {IoMdRemoveCircleOutline} from 'react-icons/io';
 
 import img from '../../assets/icons/11n.png';
 import './Favorites.css';
+import Button from '../Buttons/Button/Button';
 
 const favorites = props => (
     <div className='FavoritesCard'>
+        <Button btnType='Remove'><IoMdRemoveCircleOutline /></Button>
         <p className='City'>Ordu</p>
        <div style={{display: 'flex', alignItems: 'center'}}>
             <img src={img} alt='weather'/>
