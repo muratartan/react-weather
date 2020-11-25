7b4a6893814b4da80e0a54562ca0fa796b348cc6
murat <muratartan@hotmail.com>
2020-11-25 16:28:13 +0300

    style forecast tab navigator

--- src/components/TabNavigator/TabNavigator.js
+++ src/components/TabNavigator/TabNavigator.js
@@ -3,38 +3,66 @@ import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import {AiOutlineClockCircle} from 'react-icons/ai';
-import {FiCalendar} from 'react-icons/fi';
+import CalendarTodayOutlinedIcon from '@material-ui/icons/CalendarTodayOutlined';
+import ScheduleOutlinedIcon from '@material-ui/icons/ScheduleOutlined'
+
+import './TabNavigator.css';
+
+
 
 const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-    maxWidth: 500,
-  },
+    root: {
+        flexGrow: 1,
+        width:600,
+        backgroundColor:'transparent',
+        boxShadow:'none'
+    },
+    tab:{
+        color:'#7d7777'
+    },
+    tabActive:{
+        color:'whitesmoke'
+    }
 });
 
-export default function IconLabelTabs() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
 
-  return (
-    <Paper square className={classes.root}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        variant="fullWidth"
-        indicatorColor="secondary"
-        textColor="secondary"
-        aria-label="icon label tabs example"
-      >
-        <Tab icon={<AiOutlineClockCircle />} label="Hourly" />
-        <Tab icon={<FiCalendar />} label="Weekly" />
-        
-      </Tabs>
-    </Paper>
-  );
+const IconLabelTabs = () => {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Paper square className={classes.root}>
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                variant="fullWidth"
+                indicatorColor="inherit"
+                textColor="inherit"
+                aria-label="icon label tabs example"
+                className='Tabs'
+            >
+                <Tab
+                    classes={{
+                        selected:classes.tabActive,
+                        textColorInherit:classes.tab
+                    }}
+                    icon={<ScheduleOutlinedIcon/>}
+                />
+                <Tab
+                    classes={{
+                        selected:classes.tabActive,
+                        textColorInherit:classes.tab
+                    }}
+                    icon={<CalendarTodayOutlinedIcon/>}
+                />
+            </Tabs>
+        </Paper>
+    );
 }
+
+export default IconLabelTabs;
