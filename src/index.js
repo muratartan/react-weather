334e802e0e5a9210eef55ef73180a5c19f8aa06b
murat <muratartan@hotmail.com>
2020-11-26 20:59:02 +0300

    connect the app to the redux store

--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+
+import reducer from './store/reducers/weather';
+
+const store = createStore(reducer,applyMiddleware(thunk));
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
+      <Provider store={store}>
       <App />
+      </Provider>
     </React.StrictMode>
   </BrowserRouter>
   ,
