d30fd825890be7b7290bf9ef15daef2bc2e80b91
murat <muratartan@hotmail.com>
2020-12-04 18:11:07 +0300

    make Favorites responsive

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -6,17 +6,18 @@ import './Favorites.css';
 import Button from '../Buttons/Button/Button';
 
 const favorites = props => (
-    <div className='FavoritesContainer'>
-        <Button btnType='Remove' clicked={props.clicked}><IoMdRemoveCircleOutline /></Button>
-        <div className='FavoritesCard' onClick={props.showFavoriteCurrentWeather}>
-            <p className='City'>{props.locationName}</p>
+    <div className='FavoritesCard'>
+        <div className='CityInfoContainer' onClick={props.showFavoriteCurrentWeather}>
+            <div>
+                <p className='City'>{props.locationName}</p>
+            </div>
             <div style={{display: 'flex', alignItems: 'center'}}>
                 <img src={img} alt='weather'/>
                 <p className='Temperature'>25</p>
             </div>
         </div>
+        <Button btnType='Remove' clicked={props.clicked}><IoMdRemoveCircleOutline /></Button>
     </div>
-   
 );
 
 export default favorites;
