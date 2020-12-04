143284db0e716b852819d066a4f67b5b6a86f607
murat <muratartan@hotmail.com>
2020-12-04 23:16:57 +0300

    redirect to home page when clicking app logo in header

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom'
 
 import './Logo.css';
 import AppLogo from '../../assets/logo.png';
 
 const logo = props => (
-    <div className='Logo' style={{height: props.height}}>
+    <NavLink to='/' activeStyle={{border: 'transparent'}}>
+        <div className='Logo' style={{height: props.height}}>
         <img src={AppLogo} alt='app-logo' />
         <div className='App-name'>
             <p className='Logo-name'>MRT-DEV</p>
             <p className='App-type'>weather</p>
         </div>
     </div>
+    </NavLink>
 );
 
 export default logo;
